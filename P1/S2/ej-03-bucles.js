b480504163705880ba3c8292eaf49b6576339f91
jprado1999 <j.prado.2017@alumnos.urjc.es>
2021-02-23 16:25:02 +0100

    Ejercicio 1: Javascript

---
+++ P1/S2/ej-03-bucles.js
@@ -0,0 +1,15 @@
+//-- Ejemplo de bucles
+
+//-- Definiendo una constante: Número de mensajes
+const N = 10; 
+
+//-- Bucle para imprimir N mensajes
+for (i = 0; i < N; i++) {
+    console.log("Mensaje " + i);
+}
+
+console.log("")
+
+for (i = 10; i >= 0; i--) {
+    console.log("Mensaje " + i);
+}
