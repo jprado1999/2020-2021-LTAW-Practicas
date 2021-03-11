97237082646c6aeb3066653b1effae687e5abb0c
jprado1999 <j.prado.2017@alumnos.urjc.es>
2021-03-11 12:29:00 +0100

    Avances en el servidor, hacer pagina de error

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,72 +1,88 @@
+//-- Importo los módulos necesarios para el servidor
+const url = require('url');
 const http = require('http');
 const fs = require('fs');
 
+//-- Defino el numero de puerto que utilizo
 const PUERTO = 9000;
 
-//-- Texto HTML de la página principal
-const pagina_main = `
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>El baúl del coleccionista</title>
-</head>
-<body style="background-color: lightblue">
-    <h1 style="color: green">EL BAÚL DEL COLECCIONISTA</h1>
-    <img src="img/seccion4k.jpg" alt="Imagen">
-</body>
-</html>
-`
-
-//-- Texto HTML de la página de error
-const pagina_error = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>El baúl del coleccionista</title>
-</head>
-<body style="background-color: red">
-    <h1 style="color: white">ERROR!!!!</h1>
-</body>
-</html>
-`
-
-const server = http.createServer((req, res)=>{
+//-- Creo el servidor
+const server = http.createServer((req, res) => {
     console.log("Petición recibida!");
 
     //-- Valores de la respuesta por defecto
     let code = 200;
     let code_msg = "OK";
-    let page = pagina_main;
+    let page = "./tienda.html";
 
-    //-- Analizar el recurso
-    //-- Construir el objeto url con la url de la solicitud
+    //-- Declaro el valor del content type por defecto
+    let mimetype = 'text/html';
+
+    //-- Declaro la variable que almacenará los recursos solicitados
+    let petition = "";
+
+    //-- Construyo la url que pide el cliente
     const url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log(url.pathname);
+    //console.log(url.pathname);
 
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    /*if (url.pathname != '/') {
-        code = 404;
-        code_msg = "Not Found";
-        page = pagina_error;
-    }*/
+    //-- Si se pide la pagina principal
+    if (url.pathname == "/") {
+        petition += "/tienda.html";
+    } else if (url.pathname == '/favicon.ico') {    //-- Si se pide el icono de la pestaña
+        petition += '/img/carrito.jpg';
+    } else {                                        //-- Si se pide cualquier otra cosa
+        petition = url.pathname;
+    }
+
+    //-- Me guardo el tipo de recurso pedido
+    resource = petition.split(".")[1];
+    petition = "." + petition;
+
+    //console.log("Nombre del fichero: " + petition);
+    //console.log("Tipo de recurso: " + resource);
 
     //-- Generar la respusta en función de las variables
     //-- code, code_msg y page
     res.statusCode = code;
     res.statusMessage = code_msg;
-    res.setHeader('Content-Type','text/html');
-    res.write(String(page));
-    res.end();
+
+    //-- Lectura asincrona de los recursos a mostrar en la pagina
+    fs.readFile(petition, (err, data) => {
+        if (err) {
+            res.statusCode = 404
+            res.statusMessage = "Not Found"
+            petition = "./error.html";
+            data = fs.readFileSync(petition);
+            res.setHeader('Content-Type', mimetype);
+            res.write(data);
+            return res.end();
+        }
+
+        //-- El valor de content type varia segun el formato de imagen pedido
+        if (resource == 'jpg' || resource == 'png' || resource == 'jpeg') {
+            mimetype = 'image/' + resource;
+        }
+
+        //-- Si nos piden el iconito, su tipo es texto plano
+        if (resource == "ico") {
+            mimetype = 'text/plain';
+        }
+
+        //-- Ajusto el valor del content type cuando nos piden un css
+        if (resource == "css") {
+            mimetype = 'text/css';
+        }
+
+        //console.log(mimetype)
+
+        //-- Escribo la cabecera del mensaje y muestro la pagina solicitada
+        res.setHeader('Content-type', mimetype);
+        res.write(data);
+        res.end();
+    });
 });
 
+//-- Inicio el servidor en el puerto 9000
 server.listen(PUERTO);
 
 console.log("Servidor back-end. Escuchando en puerto: " + PUERTO);
