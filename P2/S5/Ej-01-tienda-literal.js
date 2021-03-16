c009fb4b3baee07426ffa0273bb84ae77393f26d
jprado1999 <j.prado.2017@alumnos.urjc.es>
2021-03-16 14:44:20 +0100

    Ejercicios S5

---
+++ P2/S5/Ej-01-tienda-literal.js
@@ -0,0 +1,30 @@
+//-- Crear la variable tienda, que es una estructura formada
+//-- por dos productos
+const tienda = [
+    {
+      nombre: "Alhambra II",
+      descripcion: "Placa con FPGA ice40HX8K",
+      stock: 3
+    },
+    {
+      nombre: "Ice",
+      stock: 2
+    },
+    {
+      nombre: "Ices",
+      stock: 1
+    },
+    {
+      nombre: "Icestick",
+      stock: 10
+    }
+  ];
+  
+  //-- Mostrar informacion sobre la tienda
+  console.log("Productos en la tienda: " + tienda.length);
+  
+  //-- Recorrer el array de productos
+  tienda.forEach((element, index)=>{
+    console.log("Producto: " + (index + 1) + ": " + element.nombre);
+    console.log("Stock: " + element.stock);
+  });
---
+++ P2/S5/Ej-02.js
@@ -0,0 +1,33 @@
+//-- Cadena con la estructura de la tienda en JSON
+const tienda_json = `[
+    {
+      "nombre": "Alhambra II",
+      "descripcion": "Placa con FPGA ice40HX8K",
+      "stock": 3
+    },
+    {
+      "nombre": "Icestick",
+      "stock": 10
+    },
+    {
+      "nombre": "Ice",
+      "stock": 3
+    },
+    {
+      "nombre": "Ices",
+      "stock": 4
+    }
+  ]`
+  
+  //-- Crear la estructura tienda a partir de la cadena en json
+  const tienda = JSON.parse(tienda_json);
+  
+  //-- Mostrar informacion sobre la tienda
+  console.log("Productos en la tienda: " + tienda.length);
+  
+  //-- Recorrer el array de productos
+  tienda.forEach((element, index)=>{
+    console.log("Producto: " + (index + 1) + ": " + element["nombre"]);
+    console.log("Stock: " + element["stock"]);
+  });
+  
