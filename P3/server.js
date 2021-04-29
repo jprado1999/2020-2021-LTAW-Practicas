99b8eac595b8090d1d648ce4cf0a1dea459ede62
jprado1999 <j.prado.2017@alumnos.urjc.es>
2021-04-29 17:06:32 +0200

    Primeros pasos

--- P3/server.js
+++ P3/server.js
@@ -15,10 +15,15 @@ const server = http.Server(app);
 //-- Crear el servidor de websockets, asociado al servidor http
 const io = socket(server);
 
+const comandos = "<p style='color:red'>/help: Devuelve una lista con los comandos disponibles</p> \
+<p style='color:red'>/list: Devuelve el número de usuarios conectados</p> \
+<p style='color:red'>/hello: Devuelve un saludo por parte del servidor</p> \
+<p style='color:red'>/date: Devuelve la fecha actual</p>";
+
 //-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Definir el punto de entrada principal de mi aplicación web
 app.get('/', (req, res) => {
-  res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/intro.html">Entrar al Chat</a></p>');
+    res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/intro.html">Entrar al Chat</a></p>');
 });
 
 //-- Esto es necesario para que el servidor le envíe al cliente la
@@ -31,21 +36,39 @@ app.use(express.static('public'));
 //------------------- GESTION SOCKETS IO
 //-- Evento: Nueva conexion recibida
 io.on('connect', (socket) => {
+
+    //-- Envio un mensaje a todos los clientes anunciando que alguien se ha unido
+    io.send("Un nuevo usuario se ha unido al Chat");
+
+    //-- Mensaje de bienvenida, solo lo ve el cliente que se conecta
+    socket.send("Has entrado en el Chat");
   
-  console.log('** NUEVA CONEXIÓN **'.yellow);
+    console.log('** NUEVA CONEXIÓN **'.yellow);
 
-  //-- Evento de desconexión
-  socket.on('disconnect', function(){
-    console.log('** CONEXIÓN TERMINADA **'.yellow);
-  });  
+    //-- Evento de desconexión
+    socket.on('disconnect', function(){
+        console.log('** CONEXIÓN TERMINADA **'.yellow);
+    });  
 
-  //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
-  socket.on("message", (msg)=> {
-    console.log("Mensaje Recibido!: " + msg.blue);
+    //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
+    socket.on("message", (msg)=> {
+        console.log("Mensaje Recibido!: " + msg.blue);
 
-    //-- Reenviarlo a todos los clientes conectados
-    io.send(msg);
-  });
+        //-- Un cliente teclea un comando
+        if (msg == '/help') {
+            socket.send(comandos);
+        } else if (msg == '/list') {
+            socket.send("Lista de usuarios conectados")
+        } else if (msg == '/hello') {
+            socket.send("Heeey I'm the server. How are you?")
+        } else if (msg == '/date') {
+            socket.send("Hoy es: ")
+        } else {
+            //-- Reenvio el mensaje a todos los clientes conectados
+            io.send(msg);
+        }
+
+    });
 
 });
 
