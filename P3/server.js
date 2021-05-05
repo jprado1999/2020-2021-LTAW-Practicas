8d78d723502f0a29facc07d4b301aa0ccc8a82ca
jprado1999 <j.prado.2017@alumnos.urjc.es>
2021-05-05 13:13:06 +0200

    Mejora lista de usuarios

--- P3/server.js
+++ P3/server.js
@@ -9,6 +9,9 @@ const PUERTO = 8080;
 //-- Defino una variable para almacenar la cantidad de usuarios
 let users = 0;
 
+//-- Defino un array que almacenará los nicks
+let nicknamesArray = [];
+
 //-- Mensaje con comandos para cuando lo pida un cliente
 const comandos = "<p style='color:red'>/help: Devuelve una lista con los comandos disponibles</p> \
 <p style='color:red'>/list: Devuelve el número de usuarios conectados</p> \
@@ -50,30 +53,39 @@ io.on('connect', (socket) => {
   
     console.log('** NUEVA CONEXIÓN **'.yellow);
 
-    console.log("Usuarios: " + users);
-
-    //-- Evento de desconexión
-    socket.on('disconnect', function(){
-        console.log('** CONEXIÓN TERMINADA **'.yellow);
-
-        //-- Decremento en 1 a los usuarios del chat
-        users -= 1;
-
-        //-- Envio un mensaje a todos los usuarios para informar
-        io.send("Un usuario ha abandonado el Cyber-Chat");
-
-        console.log("Usuarios: " + users);
-    });  
+    console.log("Usuarios: " + users);  
 
     //-- Mensaje para todos los usuarios
     socket.on('nick', (nick) => {
         io.send(nick + " ha entrado en el Cyber-Chat");
+
+        //-- Incluyo el nuevo nick en el array
+        nicknamesArray.push(nick);
+        console.log(nicknamesArray);
+
+        //-- Evento de desconexión
+        socket.on('disconnect', function(){
+            console.log('** CONEXIÓN TERMINADA **'.yellow);
+
+            //-- Decremento en 1 a los usuarios del chat
+            users -= 1;
+
+            //-- Envio un mensaje a todos los usuarios para informar
+            io.send(nick + " ha abandonado el Cyber-Chat");
+
+            //-- Elimino a ese cliente del Array
+            let position = nicknamesArray.indexOf(nick);
+            nicknamesArray.splice(position, 1);
+
+            console.log("Usuarios: " + users);
+        });
 
         //-- Mensaje recibido: Va dirigido a todos los clientes
         socket.on("msg", (msg)=> {
             console.log("Mensaje Recibido!: " + msg.blue);
             
             //-- Reenvio el mensaje a todos los clientes conectados
+            //-- Añadiendo el nick de quien lo envia
             msg = nick + ": " + msg;
             io.send(msg);
         });
@@ -92,8 +104,10 @@ io.on('connect', (socket) => {
                 case '/list':
                     if (users > 1) {
                         socket.send("Actualmente hay " + users + " usuarios conectados");
+                        socket.send("Usuarios: " + nicknamesArray);
                     } else {
                         socket.send("Actualmente hay " + users + " usuario conectado");
+                        socket.send("Usuario: " + nicknamesArray);
                     } 
                     break;
                 case '/hello':
